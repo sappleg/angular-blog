e315759d47c40dbcbb98d1e4faae84ba76769428
Spencer Applegate <spencer.applegate3@gmail.com>
2013-05-22 22:56:09 -0400

    Reversed blog post orders and incorporated bootstrap

--- app/js/filters.js
+++ app/js/filters.js
@@ -7,4 +7,9 @@ angular.module('SpencerApplegateBlog.filters', [])
         return function(text) {
             return String(text).replace(/\%VERSION\%/mg, version);
         }
+    }])
+    .filter('reverse', [function() {
+        return function(items) {
+            return items.slice().reverse();
+        }
     }]);
