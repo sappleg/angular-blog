f8e5881c5d539535659a8b0db253e3932d892d80
Spencer Applegate <spencer.applegate3@gmail.com>
2013-07-31 16:40:02 -0400

    Initialized post edit unit test file

---
+++ src/app/blog/posts/edit/edit.unit.js
@@ -0,0 +1,18 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: spencer
+ * Date: 7/14/13
+ * Time: 1:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+'use strict';
+
+describe('posts.edit', function() {
+	
+    beforeEach(function() {
+		module('posts.edit');
+    });
+
+    
+});
