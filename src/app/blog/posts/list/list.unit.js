fce4194be8503f217333406d24c7e73704793fd3
Spencer Applegate <spencer.applegate3@gmail.com>
2013-08-02 16:57:21 -0400

    Added post list unit test template

---
+++ src/app/blog/posts/list/list.unit.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: spencera
+ * Date: 8/2/13
+ * Time: 12:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+'use strict';
+
+describe('posts.list', function() {
+
+});
